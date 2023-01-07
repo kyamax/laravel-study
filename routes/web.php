ca0c8f84c418e947dce61c77bc0360b3492e5795
kyamax <kisho.0125@gmail.com>
2023-01-07 12:04:01 +0900

    viewの追加

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/hello_world', fn() => view('hello_world'));
 // Route::get('/hello_world', function(){
 //     return view('hello_world');
@@ -28,3 +24,7 @@ Route::get('/hello', fn() => view('hello',[
     'course' => 'laravel9'
     ]
 ));
+
+Route::get('/', fn() => view('index'));
+
+Route::get('/curriculum', fn() => view('curriculum'));
