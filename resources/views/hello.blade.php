86028e8f36a7de3dc41f95c211c39af8490c6279
kyamax <kisho.0125@gmail.com>
2023-01-07 11:46:42 +0900

    route・viewの表示

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<div>
+  hello world
+</div>
+<p>こんにちは、{{ $name }}さん</p>
+<p>本コースは{{ $course }}です</p>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello_world', fn() => view('hello_world'));
+// Route::get('/hello_world', function(){
+//     return view('hello_world');
+// });
+
+
+Route::get('/hello', fn() => view('hello',[
+    'name' => 'yama',
+    'course' => 'laravel9'
+    ]
+));
